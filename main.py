4300508bbacf42f40f65b02ac9a3cbdd6b580fa2
Rahul Verma <95970334+rahulhbtu@users.noreply.github.com>
2023-10-25 00:32:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,78 @@
 import cv2
-import numpy as np
-import pyautogui
+import numpy
 
 
+#initial function for the callin of the trackbar
+def hello(x):
+	#only for referece
+	print("")
+
+#initialisation of the camera
 cap = cv2.VideoCapture(0)
-yellow_lower = np.array([22, 93, 0])
-yellow_upper = np.array([45, 255, 255])
-prev_y = 0
+bars = cv2.namedWindow("bars")
 
-while True:
-    ret, frame = cap.read()
+cv2.createTrackbar("upper_hue","bars",110,180,hello)
+cv2.createTrackbar("upper_saturation","bars",255, 255, hello)
+cv2.createTrackbar("upper_value","bars",255, 255, hello)
+cv2.createTrackbar("lower_hue","bars",68,180, hello)
+cv2.createTrackbar("lower_saturation","bars",55, 255, hello)
+cv2.createTrackbar("lower_value","bars",54, 255, hello)
 
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv,yellow_lower, yellow_upper)
-    contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    for c in contours:
-        area = cv2.contourArea(c)
-        if area > 300:
-            x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame,(x,y), (x+w,y+h), (0, 255, 0), 2)
-            if y < prev_y:
-                pyautogui.press('space')
+#Capturing the initial frame for creation of background
+while(True):
+	cv2.waitKey(1000)
+	ret,init_frame = cap.read()
+	#check if the frame is returned then brake
+	if(ret):
+		break
 
-            prev_y = y
-    cv2.imshow('frame', frame)
+# Start capturing the frames for actual magic!!
+while(True):
+	ret,frame = cap.read()
+	inspect = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+	#getting the HSV values for masking the cloak
+	upper_hue = cv2.getTrackbarPos("upper_hue", "bars")
+	upper_saturation = cv2.getTrackbarPos("upper_saturation", "bars")
+	upper_value = cv2.getTrackbarPos("upper_value", "bars")
+	lower_value = cv2.getTrackbarPos("lower_value","bars")
+	lower_hue = cv2.getTrackbarPos("lower_hue","bars")
+	lower_saturation = cv2.getTrackbarPos("lower_saturation","bars")
 
-    if cv2.waitKey(10) == ord('q'):
-        break
-cap.release()
+	#Kernel to be used for dilation
+	kernel = numpy.ones((3,3),numpy.uint8)
+
+	upper_hsv = numpy.array([upper_hue,upper_saturation,upper_value])
+	lower_hsv = numpy.array([lower_hue,lower_saturation,lower_value])
+
+	mask = cv2.inRange(inspect, lower_hsv, upper_hsv)
+	mask = cv2.medianBlur(mask,3)
+	mask_inv = 255-mask 
+	mask = cv2.dilate(mask,kernel,5)
+	
+	#The mixing of frames in a combination to achieve the required frame
+	b = frame[:,:,0]
+	g = frame[:,:,1]
+	r = frame[:,:,2]
+	b = cv2.bitwise_and(mask_inv, b)
+	g = cv2.bitwise_and(mask_inv, g)
+	r = cv2.bitwise_and(mask_inv, r)
+	frame_inv = cv2.merge((b,g,r))
+
+	b = init_frame[:,:,0]
+	g = init_frame[:,:,1]
+	r = init_frame[:,:,2]
+	b = cv2.bitwise_and(b,mask)
+	g = cv2.bitwise_and(g,mask)
+	r = cv2.bitwise_and(r,mask)
+	blanket_area = cv2.merge((b,g,r))
+
+	final = cv2.bitwise_or(frame_inv, blanket_area)
+
+	cv2.imshow("Harry's Cloak",final)
+
+	if(cv2.waitKey(3) == ord('q')):
+		break;
+
 cv2.destroyAllWindows()
-
-
+cap.release()
